852306a8cdb9a9128444c83ecf369b781c5e5f41
Bouthaina <bouthainabenhfayedh@gmail.com>
2022-03-04 16:18:30 +0100

    add field name

--- data/src/com/test/entities/Person.java
+++ data/src/com/test/entities/Person.java
@@ -1,4 +1,13 @@
 package com.test.entities;
 
 public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
