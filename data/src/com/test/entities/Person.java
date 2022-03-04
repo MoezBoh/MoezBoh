a896c94b38401bcaf55d54d4fba08e08c112e489
Bouthaina <bouthainabenhfayedh@gmail.com>
2022-03-04 16:24:14 +0100

    add methodes

--- data/src/com/test/entities/Person.java
+++ data/src/com/test/entities/Person.java
@@ -1,5 +1,7 @@
 package com.test.entities;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
 
@@ -9,5 +11,25 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
