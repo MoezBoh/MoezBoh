27f6ae10d1c3fdcc99522195bbe179e865fb0bf9
Bouthaina <bouthainabenhfayedh@gmail.com>
2022-03-04 17:28:25 +0100

    add Edition.java

---
+++ data/src/com/test/entities/Edition.java
@@ -0,0 +1,13 @@
+package com.test.entities;
+
+public class Edition {
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
